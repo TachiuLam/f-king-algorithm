bd5f6a7813f6eaf35156e3e870db68ee37c61887
TachiuLam <techaolin@gmail.com>
2023-02-15 00:17:00 +0800

    leetcode 876、链表的中间点 快慢指针

---
+++ 链表/876、链表的中间节点.go
@@ -0,0 +1,20 @@
+package 链表
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 快慢指针解法：快指针到达末尾时，慢指针刚好到达中点
+// 时间复杂度O(N)，空间复杂度O(1)
+func middleNode(head *ListNode) *ListNode {
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		head = head.Next
+	}
+	return head
+}
