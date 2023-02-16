be871e106e0b6abb04ee132ae41406c8e5eeedbe
TachiuLam <techaolin@gmail.com>
2023-02-16 22:40:00 +0800

    leetcode 86、分隔链表 1、双链表+双指针 理解指针的定义

--- test/a_test.go
+++ test/a_test.go
@@ -1,21 +1,58 @@
 package test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
-func TestDumpPoint(t *testing.T) {
-	l3 := &ListNode{
-		Val: 4,
+func partition(head *ListNode, x int) *ListNode {
+	slow, high := &ListNode{}, &ListNode{}
+	p1, p2 := slow, high
+	i := 0
+	for head != nil {
+		i++
+		fmt.Println("循环...", i)
+		fmt.Println("head = ", head, head.Next)
+		if head.Val < x {
+			p1.Next = head
+			//fmt.Println("p1 = ", p1, p1.Next)
+			p1 = p1.Next
+			//fmt.Println("p1 = ", p1, p1.Next)
+		} else {
+			p2.Next = head
+			p2 = p2.Next
+		}
+		fmt.Println("p1 = ", p1, "p1.Next", p1.Next)
+		fmt.Println("p2 = ", p2, p2.Next)
+		fmt.Println("slow = ", slow, slow.Next, slow.Next.Next, slow.Next.Next.Next)
+		fmt.Println("high = ", high, high.Next)
+		head = head.Next
 	}
-	l1 := new(ListNode)
-	l2 := l1
-	l1.Val = 2
-	l2.Val = 3
-	l2.Next = l3
-	l2 = l2.Next
-	t.Log(l1, l2)
+	p2.Next = nil
+	p1.Next = high.Next
+	return slow.Next
+}
+
+func Test86(t *testing.T) {
+	l3 := &ListNode{
+		Val: 2,
+	}
+	l2 := &ListNode{
+		Val:  3,
+		Next: l3,
+	}
+	l1 := &ListNode{
+		Val:  4,
+		Next: l2,
+	}
+	l0 := &ListNode{
+		Val:  1,
+		Next: l1,
+	}
+	t.Log(partition(l0, 3))
 }
---
+++ 链表/86、分隔链表.go
@@ -0,0 +1,27 @@
+package 链表
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func partition(head *ListNode, x int) *ListNode {
+	slow, high := &ListNode{}, &ListNode{}
+	p1, p2 := slow, high
+	for head != nil {
+		if head.Val < x {
+			p1.Next = head
+			p1 = p1.Next
+		} else {
+			p2.Next = head
+			p2 = p2.Next
+		}
+		head = head.Next
+	}
+	p2.Next = nil
+	p1.Next = high.Next
+	return slow.Next
+}
