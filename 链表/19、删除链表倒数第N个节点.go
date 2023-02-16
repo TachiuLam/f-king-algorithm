57a564950d61de20c7a78573732de72de7aa7ad7
TachiuLam <techaolin@gmail.com>
2023-02-16 21:09:33 +0800

    leetcode 19、删除链表倒数第N个节点 1、双指针 2、不同时刻开始遍历

---
+++ 链表/19、删除链表倒数第N个节点.go
@@ -0,0 +1,29 @@
+package 链表
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 双指针：
+//1、 p1 = head; p2 = head - 1; p1 先走n步
+//2、p1,p2 同时走，p1到达链表末尾时，p2到达倒数第n个节点前
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	p3 := &ListNode{0, head}
+	p1, p2 := head, p3
+	for i := 0; i < n; i++ {
+		p1 = p1.Next
+	}
+	for p1 != nil {
+		p2 = p2.Next
+		p1 = p1.Next
+	}
+	p2.Next = p2.Next.Next
+	return p3.Next
+}
