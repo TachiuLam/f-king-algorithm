76e82566ee2f84e41644b9d2d888a2a7f2effa9a
TachiuLam <techaolin@gmail.com>
2023-02-20 22:44:50 +0800

    剑指offer 022

---
+++ 链表/剑指offer022.链表中环的入口节点.go
@@ -0,0 +1,26 @@
+package 链表
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle2(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}
