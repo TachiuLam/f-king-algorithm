1d93448761db0a26975cae9f55832117f0033e36
TachiuLam <techaolin@gmail.com>
2023-03-07 21:08:24 +0800

    leetcode 27、移除元素 1、快慢指针，保存不等项

---
+++ 数组/27、移除元素.go
@@ -0,0 +1,17 @@
+package 数组
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
