73396464991e00ab0dfb57feaa56a6270c3a1a0d
TachiuLam <techaolin@gmail.com>
2023-03-08 22:51:29 +0800

    leetcode 283、移动零 1、先删除，再补零

---
+++ 数组/283、移动零.go
@@ -0,0 +1,17 @@
+package 数组
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func moveZeroes(nums []int) {
+	slow, count := 0, 0
+	for _, item := range nums {
+		if item != 0 {
+			nums[slow] = item
+			slow++
+		} else {
+			count++
+		}
+	}
+	for i := 0; i < count; i++ {
+		nums[slow+i] = 0
+	}
+}
