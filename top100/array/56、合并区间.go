56c7b6dc25d6e04531f60a14c0e8cf11490fa7a9
TachiuLam <techaolin@gmail.com>
2024-03-05 23:32:37 +0800

    top100 55、跳跃游戏 56、合并区间

---
+++ top100/array/56、合并区间.go
@@ -0,0 +1,48 @@
+package array
+
+import "sort"
+
+/*
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，
+并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+示例 1：
+
+输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+示例 2：
+
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+提示：
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+*/
+//时间复杂度: O(nlogn)
+//空间复杂度: O(logn)，排序需要的空间开销
+// https://leetcode.cn/problems/merge-intervals/solutions/858344/dai-ma-sui-xiang-lu-dai-ni-xue-tou-tan-x-gw3a/?envType=featured-list&envId=2cktkvj?envType=featured-list&envId=2cktkvj
+
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 1 {
+		return intervals
+	}
+	// 排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	res := make([][]int, 0)
+	res = append(res, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if res[len(res)-1][1] >= intervals[i][0] { // 前一个的左边界大于右边界
+			res[len(res)-1][1] = max(res[len(res)-1][1], intervals[i][1]) // 取更大值作为pre的右边界
+		} else {
+			res = append(res, intervals[i])
+		}
+	}
+	return res
+}
